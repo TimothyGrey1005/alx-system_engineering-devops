f1e0858a0e5a7743b85777d316926dfaab404fdf
TimothyGrey1005 <omphilegopane913@gmail.com>
2023-11-26 17:35:46 -0800

    0x0B-SSH

--- 0x0B-SSH/4-puppet_ssh_config.pp
+++ 0x0B-SSH/4-puppet_ssh_config.pp
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 # Changes SSH config file
 exec { 'echo':
   path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
   returns => [0,1],
 }
